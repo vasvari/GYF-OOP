77583291008543c28696bb9e42041d7353d5e8a1
Boros Bence <borosbence6800@gmail.com>
2025-10-20 10:12:49 +0200

    init

---
+++ KarakterKodolo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KarakterKodolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tehenek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tehenek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
